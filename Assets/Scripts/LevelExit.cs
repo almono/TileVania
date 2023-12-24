d93aa6dba24f0fd12efd422225b224956c3d91d4
almono <pwalczykiewicz93@gmail.com>
2023-12-24 22:43:04 +0100

    Level Exit

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(FinishLevel());
+    }
+
+    private IEnumerator FinishLevel()
+    {
+        yield return new WaitForSecondsRealtime(1);
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
